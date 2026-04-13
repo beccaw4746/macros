82144da98a3b6d11021c2fefca3ccc81e7848467
beccaw4746 <rebecca.williams@appiphony.com>
2026-04-13 12:56:42 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'macros-v1';
+const FILES = [
+  '/macros/',
+  '/macros/index.html',
+  '/macros/manifest.json',
+  'https://fonts.googleapis.com/css2?family=DM+Mono:wght@400;500&family=DM+Sans:wght@300;400;500&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
